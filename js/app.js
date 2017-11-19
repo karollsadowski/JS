384a67c49e407052243e053db6055be474942077
karollsadowski <karoll.sadowski@yahoo.com>
2017-11-19 19:18:29 +0100

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
-var q = $("ul.people li");
+var q = $("ul.people li").addClass("newClass");
 console.log(q);
 
 var test = 'Test';
 var p = document.getElementById('p1');
 p.textContent = test + ' ' + p.textContent + ' ' + q[0].outerText;
+
+
+var g = G$('John', 'Doe');
+//console.log(g);
+g.greet().setLang('en').greet(true).log();
+
+
+$('#login').click(function() {
+   
+    var loginGrtr = G$('John', 'Doe');
+    
+    $('#logindiv').hide();
+    
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+    
+});

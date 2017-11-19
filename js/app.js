93d645b76e08124262bdddb2f3676c794d894f64
karollsadowski <karoll.sadowski@yahoo.com>
2017-11-19 17:31:11 +0100

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+var q = $("ul.people li");
+console.log(q);
+
 var test = 'Test';
 var p = document.getElementById('p1');
-p.textContent = test + ' ' + p.textContent;
+p.textContent = test + ' ' + p.textContent + ' ' + q[0].outerText;
